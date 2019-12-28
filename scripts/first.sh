697b3e53c42047b9b272cfd5fc7a40c2c055f33b
RVIRUS0817 <t.virus.1989@gmail.com>
2019-12-28 11:10:41 +0900

    fix scripts

--- scripts/first.sh
+++ scripts/first.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+## install xcode
 sudo xcodebuild -license
 sudo xcode-select --install
+
+## install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
 brew install ansible
+
+## install terminal fonts
+cd ~/ && git clone https://github.com/powerline/fonts.git
+fonts/install.sh && rm -rf ~/fonts
